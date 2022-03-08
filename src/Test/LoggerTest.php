ca4dabd479a8d95f0015acc770ab97731a7042cd
yuyasakamoto <freetairu@yahoo.co.jp>
2022-03-08 23:45:50 +0900

    失敗をテスト

--- src/Test/LoggerTest.php
+++ src/Test/LoggerTest.php
@@ -10,7 +10,7 @@ final class LoggerTest extends TestCase
     public function testA()
     {
         Logger::_print();
-        $this->assertSame("こんちわ", "こんちわ");
+        $this->assertSame("こんちわ", "こんちわa");
         // $obj = new Human();
         // $this->assertSame("こんにちわ",$obj->helloString());
     }
