03b7b417485e77cdc0b107de69ed4776ea9e22f4
yuyasakamoto <freetairu@yahoo.co.jp>
2022-03-14 23:05:54 +0900

    ルーティング処理を先頭一致のルートを追加

--- src/Router.php
+++ src/Router.php
@@ -14,6 +14,7 @@ use YLibs\Utility\YString;
 class Router
 {
     private array $routes;
+    private array $prefixMatchRoutes;
     private string $namespace;
     private YString $ys;
 
@@ -31,33 +32,98 @@ class Router
      */
     public function run()
     {
-        $url = $_SERVER["REQUEST_URI"];
+        $request_url = $_SERVER["REQUEST_URI"];
         //パラメータとurlの分解の処理
 
         // 末尾のスラッシュを強制削除
-        $url = $this->ys->delEndsWith($url, '/');
-        if ($url !== '') {
+        $request_url = $this->ys->delEndsWith($request_url, '/');
+        if ($request_url !== '') {
             //スラッシュで分割して配列にする
-            $urls = explode("/", $url);
+            $request_urls = explode("/", $request_url);
             //先頭が余分なので削除。(必ずarray[0]が空文字となるので1つ取り出して排除する)
-            array_shift($urls);
+            array_shift($request_urls);
             //末尾のキー値を取り出す
-            $lastkey = array_key_last($urls);
+            $lastkey = array_key_last($request_urls);
             //末尾の要素を?で分割して配列にして先頭だけ取り出す(/abc?a=1&b=2)→(abcを取り出して元の配列に戻す)
-            $urls[$lastkey] = explode("?", $urls[$lastkey])[0];
-            $url = "/" . implode("/", $urls);
+            $request_urls[$lastkey] = explode("?", $request_urls[$lastkey])[0];
+            $request_url = "/" . implode("/", $request_urls);
         }
-        $url = $this->ys->delEndsWith($url, "/") . "/";
+        $request_url = $this->ys->delEndsWith($request_url, "/") . "/";
 
-        if (!isset($this->routes[$url])) {
-            Logger::_print($urls, __METHOD__);
-            var_dump($url);
+        //前方一致判定（ここは変数込みのもの）
+        foreach (array_keys($this->prefixMatchRoutes) as $prefix_param) {
+            //前方部分を抜き出し
+            //:から後ろで配列分割
+            //0番目はurlの値なので1番目以降からは:付きとする
+            $prefix_param_list = explode(":", $prefix_param);
+            $prefix = $prefix_param_list[0];
+            array_shift($prefix_param_list);
+            echo "<br>■\$prefix<br>";
+            var_dump($prefix);
+            // $prefix_list = explode("/", $prefix);
+            // echo "<br>■\$prefix_list<br>";
+            // var_dump($prefix_list);
+
+            // 先頭のURL部分のみを取る
+            //前方一致判定
+            if (preg_match("{^" . $prefix . "}", $request_url)) {
+                echo "<br>■\$request_url<br>";
+                var_dump($request_url);
+
+                echo "<br>■\$prefix<br>";
+                var_dump($prefix);
+
+
+                //プレフィクスを消してリクエストパラメータのみにする
+                $request_param = $this->ys->delStartsWith($request_url, $prefix);
+                echo "<br>■\$request_param-先頭プレフィクス削除<br>";
+                var_dump($request_param);
+                $request_param = $this->ys->delEndsWith($request_param, "/");
+                echo "<br>■\$request_param-後方削除<br>";
+                var_dump($request_param);
+
+                // var_dump($request_param);
+                // var_dump(explode("/", $request_param));
+                $request_param_list = explode("/", $request_param);
+                echo "<br>■\$request_param_list<br>";
+                var_dump($request_param_list);
+
+
+                //前方一致リストの後方パラメータ部分を分割して
+                //キー値を取得するurl部分をキー値にする
+                for ($i = 0; $i < count($prefix_param_list); $i++) {
+                    $key = $this->ys->delEndsWith($prefix_param_list[$i], "/");
+                    $data = @$request_param_list[$i];
+                    $pathparam[$key] = $data;
+                }
+
+                echo "<br>■\$prefix_param_list<br>";
+                var_dump($prefix_param_list);
+                echo "<br>■\$request_param_list<br>";
+                var_dump($request_param_list);
+                echo "<br>■\$pathparam<br>";
+                var_dump($pathparam);
+
+
+                foreach ($this->prefixMatchRoutes[$prefix_param] as $class => $method) {
+                    $controller = new ($this->namespace . $class);
+                    $controller->setPathParameter($pathparam);
+                    $controller->$method();
+                }
+                return;
+            }
+        }
+
+        //完全一致判定
+        if (!isset($this->routes[$request_url])) {
+            Logger::_print($request_urls, __METHOD__);
+            var_dump($request_url);
             echo "存在しない画面です。";
             exit;
             throw new Exception("存在しないURLです");
         }
 
-        foreach ($this->routes[$url] as $class => $method) {
+        foreach ($this->routes[$request_url] as $class => $method) {
             $controller = new ($this->namespace . $class);
             $controller->$method();
         }
@@ -74,5 +140,18 @@ class Router
     public function setMapping(string $url, string $class, ?string $method)
     {
         $this->routes[$url] = [$class => $method];
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param string $url
+     * @param string $class
+     * @param string|null $method
+     * @return void
+     */
+    public function setPrefixMapping(string $url, string $class, ?string $method)
+    {
+        $this->prefixMatchRoutes[$url] = [$class => $method];
     }
 }
--- src/Utility/YString.php
+++ src/Utility/YString.php
@@ -31,4 +31,35 @@ class YString
         }
         return $result;
     }
+
+    /**
+     * 文字列の先頭が指定文字列の場合、先頭の指定文字列を削除したものを返却する
+     * 指定文字列が存在しない場合検索文字列をそのまま返却する
+     *
+     * @param string $src 検索文字列
+     * @param string $target 指定文字列
+     * @return string
+     * @throws Exception $targetが1文字以外のき
+     */
+    function delStartsWith(string $src, string $target): string
+    {
+        // echo "<br>★delStartsWidth-1<br>";
+        $length = strlen($target);
+        // echo $length;
+        if ($length === null) {
+            throw new Exception("targetがnullです。");
+        }
+
+        // echo "<br>★delStartsWidth-2<br>";
+        // var_dump(substr($src, $length));
+        $result = $src;
+        if (substr($src, 0, $length) === $target) {
+            // echo "<br>★delStartsWidth-3<br>";
+            $result = substr($src, $length);
+        }
+
+        // echo "<br>★delStartsWidth-4(\$result)<br>";
+        // var_dump($result);
+        return $result;
+    }
 }
