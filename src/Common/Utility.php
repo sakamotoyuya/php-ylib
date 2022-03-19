497c2bec052204840318f746cb2947ecf7248be3
yuyasakamoto <freetairu@yahoo.co.jp>
2022-03-20 00:37:19 +0900

    ランダムキーを取得する処理を追加

--- src/Common/Utility.php
+++ src/Common/Utility.php
@@ -13,4 +13,14 @@ class Utility
         header('Location:' .  $url);
         exit;
     }
+
+    /**
+     * ランダムの暗号化キーを取得する
+     *
+     * @return string
+     */
+    static public function getRandomKey(): string
+    {
+        return sha1(uniqid(mt_rand(), true));
+    }
 }
