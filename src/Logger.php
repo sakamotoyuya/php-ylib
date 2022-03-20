475b6318d285a3773d3d915a41b9f79c03971d6d
yuyasakamoto <freetairu@yahoo.co.jp>
2022-03-20 19:32:03 +0900

    Loggerの引数位置を変更

--- src/Logger.php
+++ src/Logger.php
@@ -6,7 +6,7 @@ use Exception;
 
 class Logger
 {
-    public static function _print($variable = "", $method = "")
+    public static function _print($method = "", $variable = "")
     {
 
         //ログファイルへの書き込み処理
