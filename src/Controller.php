b6d722d135f36efc0515bed2e7c1451a45db6adb
yuyasakamoto <freetairu@yahoo.co.jp>
2022-03-25 20:43:07 +0900

    pathparameterが見つからないときはnullを返すように修正

--- src/Controller.php
+++ src/Controller.php
@@ -43,7 +43,7 @@ abstract class Controller
      */
     public function getPathParameter(string $key): mixed
     {
-        return $this->pathParameter[$key];
+        return @$this->pathParameter[$key];
     }
 
     /**
